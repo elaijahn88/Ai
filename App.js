ee003f1b5c74ffec069a4d6187de0d6d91dd53fd
elaijahn88 <elajahn8@gmail.com>
2025-08-02 11:13:31 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactPlayer from 'react-player';
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>React Video Player Example</h2>
+      <ReactPlayer
+        url="https://www.w3schools.com/html/mov_bbb.mp4"
+        controls={true}
+        width="100%"
+        height="400px"
+      />
+    </div>
+  );
+}
+
+export default App;
